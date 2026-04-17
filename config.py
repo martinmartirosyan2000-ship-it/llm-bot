0131751a331e3b2c8295a919a4c857d4f926fb2f
Martin Martirosyan <martin.martirosyan2000@gmail.com>
2026-04-18 02:00:42 +0400

    fix token

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-import os
-
-BOT_TOKEN = os.environ.get("8709953943:AAGHnnHFqL9YCI0wjnN2eZNr4hxc0i3LdMU")
-CHANNEL_ID = os.environ.get("@armintelligence")
+BOT_TOKEN = "8709953943:AAGHnnHFqL9YCI0wjnN2eZNr4hxc0i3LdMU"
+CHANNEL_ID = "@armintelligence"
